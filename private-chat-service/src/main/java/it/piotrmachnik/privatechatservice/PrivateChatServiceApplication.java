7b3b29e078bb654c7fc0d274a0a5af5dc760ad0a
Piotr Machnik <machnik.piotr2@gmail.com>
2022-10-18 17:15:58 +0200

    Files For Private Chat Service

---
+++ private-chat-service/src/main/java/it/piotrmachnik/privatechatservice/PrivateChatServiceApplication.java
@@ -0,0 +1,13 @@
+package it.piotrmachnik.privatechatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatServiceApplication.class, args);
+	}
+
+}
